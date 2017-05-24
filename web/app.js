1d230b80c3bbe4d9b989828a38984d3c04132af6
Guillaume Massé <masgui@gmail.com>
2017-05-24 12:05:14 +0200

    code side-by-side

--- web/app.js
+++ web/app.js
@@ -1,7 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   Reveal.initialize({
-    // controls: false,
-    // slideNumber: 'c/t',
+    width: 1024,
+    height: 700,
+    margin: 0.1,
+    minScale: 0.2,
+    maxScale: 1.5,
+
     history: true,
     dependencies: [
       { 
